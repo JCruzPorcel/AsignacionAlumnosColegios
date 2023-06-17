2dd83408a38974f704762a1e4c5c081faafef472
Porcel Juan Cruz <115204062+JCruzPorcel@users.noreply.github.com>
2023-06-17 00:04:07 -0300

    Update
    Se agregaron colores y nuevas funciones al código.

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,8 @@
-﻿using JCruzPorcel.Inscipcion_Colegio.Models.AlumnoData; // Utilizo el namespace para hacer referencia a los archivos que estoy utilizando, como la clase Alumno
+﻿using JCruzPorcel.Inscipcion_Colegio.Models.AlumnoData; // Utilizo el namespace para hacer referencia a los archivos que estoy utilizando, como la clase Alumno.
 using JCruzPorcel.Inscipcion_Colegio.Models.ColegioData;
-using SpreadsheetLight;
-using System.Text.RegularExpressions;
+using Spectre.Console; // Esta libraria agrega nuevas funcionalidades de consola, permitiendo así la personalizacion y mejorando la experiencia de usuario.
+using SpreadsheetLight; // Esta libreria se utiliza para poder acceder a los archivos Excel.
+using System.Text.RegularExpressions; // Esta nos permite comprar cadenas de textos, y verificar si son iguales o contienen expresiones.
 
 namespace JCruzPorcel.Inscipcion_Colegio.src // Utilizo este namespace para tener mas organización y que se vea quien es el autor.
 {
@@ -13,10 +14,12 @@ namespace JCruzPorcel.Inscipcion_Colegio.src // Utilizo este namespace para tene
     /// Hecho en Visual Studio 2022 en C# por Perez Porcel Juan Cruz.
     /// </summary> 
 
+    // Al ejecutarse se crearan las carpetas y archivos necesarios o faltantes dentro de la ruta del proyecto en la carpeta data/GeneratedFiles
+
     internal class Program // La clase Program se declara como "internal" para restringir su acceso desde programas o clases externas al proyecto actual. Esta clase contiene la lógica principal del programa.
     {
         #region Variables
-        // Nombre de la Carpeta y Archivos
+        // Nombre de las Carpetas y Archivos
         const string FOLDER = "data";
         const string INSCRIPCIONES_FILE_PATH = "Inscripciones.xlsx";
         const string ANGUIL_FILE_PATH = "Colegio_Anguil.xlsx";
@@ -38,33 +41,63 @@ namespace JCruzPorcel.Inscipcion_Colegio.src // Utilizo este namespace para tene
 
             // Ruta del archivo de plantilla
             // Combina la ruta del proyecto con la carpeta y el nombre de los archivos.
-            string inscripcionesFilePath = @Path.Combine(basePath, FOLDER, INSCRIPCIONES_FILE_PATH);  // El @ en "Path.Combine" es para tomar de forma literal la cadena de TEXTO (String).
-            string anguilFilePath = @Path.Combine(basePath, FOLDER, ANGUIL_FILE_PATH);        // No es necesario, pero lo hice para evitar posibles errores con las extenciones.                                                                                               
-            string toayFilePath = @Path.Combine(basePath, FOLDER, TOAY_FILE_PATH);           // Por ejemplo: .xlsx
+            string inscripcionesFilePath = @Path.Combine(basePath, FOLDER, INSCRIPCIONES_FILE_PATH);  // El @ en "Path.Combine" es para tomar de forma literal la cadena de texto (String).
+            string anguilFilePath = @Path.Combine(basePath, FOLDER, ANGUIL_FILE_PATH);        // No es necesario, pero lo hice para evitar posibles errores con las extenciones o las diagonales.                                                                                               
+            string toayFilePath = @Path.Combine(basePath, FOLDER, TOAY_FILE_PATH);           // Por ejemplo: .xlsx, / o \.
             string santaRosaFilePath = @Path.Combine(basePath, FOLDER, SANTA_ROSA_FILE_PATH);
             string outputFilePath = @Path.Combine(basePath, FOLDER, OUTPUT_FILE_FOLDER);
 
             List<Alumno> alumnos = LeerArchivoInscripciones(inscripcionesFilePath);
             List<Colegio> colegios = LeerArchivoColegios(toayFilePath, santaRosaFilePath, anguilFilePath);
 
-            AsignarAlumnosAColegios(alumnos, colegios);
+            string? option;
+            Colegio? colegioSeleccionado = null; // Tipo anulable
 
-            foreach (Colegio colegio in colegios)
+            do
             {
-                string outputFileName = $"Vacantes_{colegio.Nombre}.xlsx";
-                CrearArchivoVacantesColegio(colegio.AlumnosAsignados, outputFileName, outputFilePath);
+                option = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                    .Title("[underline purple]¿A qué colegio desea asignar?[/]")
+                    .AddChoices("[yellow]Anguil[/]", "[yellow]Santa Rosa[/]", "[yellow]Toay[/]", "[red]Salir[/]"));
 
-                Console.WriteLine($"Proceso finalizado. Se han generado los archivos de vacantes para el colegio {colegio.Nombre}.");
-                Console.WriteLine("\nPresiona cualquier tecla para continuar...\n\n");
-                Console.ReadKey();
-            }
+                AsignarAlumnosAColegios(alumnos, colegios, option);
+
+                colegioSeleccionado = colegios.FirstOrDefault(colegio =>
+                    Regex.IsMatch(option, @"\b" + Regex.Escape(colegio.Nombre) + @"\b", RegexOptions.IgnoreCase));
+
+
+                if (colegioSeleccionado is not null)
+                {
+                    try
+                    {
+                        string outputFileName = $"Vacantes_{colegioSeleccionado.Nombre}.xlsx";
+                        CrearArchivoVacantesColegio(colegioSeleccionado.AlumnosAsignados, outputFileName, outputFilePath);
+
+                        AnsiConsole.Markup($"[yellow]Proceso finalizado. Se han generado los archivos de vacantes para el colegio[/] [underline blue]{colegioSeleccionado.Nombre}[/].");
+                        AnsiConsole.Markup("\n\n[yellow]Se generaron todos los archivos de vacantes con[/] [green]éxito[/].");
+                        AnsiConsole.Markup("\n\nPresiona cualquier tecla para continuar...\n\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        AnsiConsole.Markup($"[underline red]Ocurrió un error al generar los archivos de vacantes:[/] {ex.Message}");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    AnsiConsole.Markup("[underline red]No se encontró el colegio seleccionado.[/]");
+                }
+            } while (option != "[red]Salir[/]");
+
 
             Console.Clear();
+            Environment.Exit(0);
 
             // Esperar a que el usuario presione una tecla para salir
-            Console.WriteLine("Se generaron todos los archivos de vacantes con EXITO.");
-            Console.WriteLine("\n\nPresiona cualquier tecla para salir...");
-            Console.ReadKey();
+            /*Console.WriteLine("\n\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();*/
+
             #endregion
 
             #region Method's
@@ -152,19 +185,30 @@ namespace JCruzPorcel.Inscipcion_Colegio.src // Utilizo este namespace para tene
                 return colegios;
             }
 
-            static void AsignarAlumnosAColegios(List<Alumno> alumnos, List<Colegio> colegios)
+            // Se cambio la forma en al que se Asignan los Alumnos a los colegios. Se agrego string colegioSeleccionado, asi se ajusta al nuevo codigo y libreria agregada Spectre.Console.
+            static void AsignarAlumnosAColegios(List<Alumno> alumnos, List<Colegio> colegios, string colegioSeleccionado)
             {
                 foreach (Alumno alumno in alumnos)
                 {
                     foreach (Colegio colegio in colegios)
                     {
-                        if (Regex.IsMatch(alumno.Preferencia, @"\b" + Regex.Escape(colegio.Nombre) + @"\b", RegexOptions.IgnoreCase) // Esto verifica que los nombres de los colegios sean los mismos que su preferencia
-                                                                                                                                     // comprobando si contiene en alguna parte la misma cadena de strings
-                            && colegio.TieneVacantes(alumno.Grado))
+                        if (Regex.IsMatch(colegioSeleccionado, @"\b" + Regex.Escape(colegio.Nombre) + @"\b", RegexOptions.IgnoreCase))
+                        {
+                            // Esto verifica que los nombres de los colegios sean los mismos que su preferencia comprobando si contiene en alguna parte la misma cadena de strings
+                            if (Regex.IsMatch(alumno.Preferencia, @"\b" + Regex.Escape(colegio.Nombre) + @"\b", RegexOptions.IgnoreCase) && colegio.TieneVacantes(alumno.Grado))
+                            {
+                                colegio.AgregarAlumno(alumno);
+                                break;
+                            }
+                        }
+
+                        /*if (Regex.IsMatch(alumno.Preferencia, @"\b" + Regex.Escape(colegio.Nombre) + @"\b", RegexOptions.IgnoreCase) // Esto verifica que los nombres de los colegios sean los mismos que su preferencia
+                            && colegio.TieneVacantes(alumno.Grado))                                                                   // comprobando si contiene en alguna parte la misma cadena de strings
+                           
                         {
                             colegio.AgregarAlumno(alumno);
                             break;
-                        }
+                        }*/
                     }
                 }
             }
@@ -197,7 +241,7 @@ namespace JCruzPorcel.Inscipcion_Colegio.src // Utilizo este namespace para tene
 
                 worksheet.RenameWorksheet(SLDocument.DefaultFirstSheetName, "Vacantes"); // Renombra la hoja de Excel a "Vacante".
 
-                worksheet.SaveAs(Path.Combine(pathFile, outputFileName));
+                worksheet.SaveAs(@Path.Combine(pathFile, outputFileName));
             }
 
         }
